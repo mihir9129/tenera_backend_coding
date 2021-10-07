74979c80ce6d6ea1e4a506effadf775471d69d11
mihir <mihirakhandhar@gmail.com>
2021-10-07 22:07:16 +0530

    Initial changes after project creation

---
+++ src/main/java/com/tenera/challenge/cityweatherinformation/CityweatherinformationApplication.java
@@ -0,0 +1,13 @@
+package com.tenera.challenge.cityweatherinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityweatherinformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityweatherinformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenera/challenge/cityweatherinformation/CityweatherinformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenera.challenge.cityweatherinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityweatherinformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
